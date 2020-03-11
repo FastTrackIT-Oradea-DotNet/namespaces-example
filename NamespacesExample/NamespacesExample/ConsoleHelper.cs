14f83fade5e095f1c99dc5c9faf3d6b24308e214
Florin Lazar <florin.lazar.it2014@gmail.com>
2020-03-11 13:57:05 +0200

    use global alias to access conflicting type names

---
+++ NamespacesExample/NamespacesExample/ConsoleHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace NamespacesExample
+{
+    namespace System
+    {
+        namespace Console
+        {
+            public class ConsoleHelper
+            {
+                public static void PrintCoordinates(TwoD.Point point)
+                {
+                    if (point is null)
+                    {
+                        return;
+                    }
+
+                    global::System.Console.WriteLine($"Point X={point.X}, Y={point.Y}");
+                }
+            }
+        }
+    }
+}
